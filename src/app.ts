b18987426be6c8b78de7ab42bdc0713f3329e203
useStateEthan <ethancampbellhodges@gmail.com>
2021-07-29 15:10:42 -0400

    updated app.ts to work without config

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express, { Router } from "express";
-import config from "config";
+//import config from "config";
+
 import log from "./logger";
 //import connect from "./db/connect";
 import routerSample from "./routers/testerRoute";
 import classDataRouter from "./routers/classDataRoute";
 
-const port = config.get("port") as number;
+//const port = config.get("port") as number;
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/test", routerSample);
 app.use("/class_data", classDataRouter);
-
+const port = 3000
 app.listen(port, () => {
 	log.info(`Server is listing on http://localhost:${port}`);
 	//connect();
